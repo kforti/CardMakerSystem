912f0d4f2517ee904edadf48db101e57f4887a04
Kevin <kevin.r.fortier@gmail.com>
2019-10-25 11:38:39 -0400

    frontend, started lambda

---
+++ cms_frontend/cards.js
@@ -0,0 +1,18 @@
+require('swagger_card_maker_system_api')
+const URL_CARDS = "";
+
+
+function getCards() {
+    console.log("hello world!")
+}
+
+function createCard() {
+
+}
+
+function deleteCard() {
+
+}
+
+
+getCards()
