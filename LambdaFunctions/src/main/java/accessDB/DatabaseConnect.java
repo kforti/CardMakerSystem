4dde45a5107ff9c072a869893ab31b88e1ff3907
Milap <milap1998@yahoo.com>
2019-10-29 20:19:36 -0400

    connect to data base class added

---
+++ LambdaFunctions/src/main/java/accessDB/DatabaseConnect.java
@@ -0,0 +1,45 @@
+package accessDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnect
+{
+	//TODO: DO NOT USE AS IS
+	// These are to be configured and NEVER stored in the code.
+	// once you retrieve this code, you can update
+	public final static String rdsMySqlDatabaseUrl = "database-2.cpwe1rxpzibp.us-east-2.rds.amazonaws.com";
+	public final static String dbUsername = "admin_republic";
+	public final static String dbPassword = "";
+		
+	public final static String jdbcTag = "jdbc:mysql://";
+	public final static String rdsMySqlDatabasePort = "3306";
+	public final static String multiQueries = "?allowMultiQueries=true";
+	   
+	public final static String dbName = "innodb";    // default created from MySQL WorkBench
+
+	// pooled across all usages.
+	static Connection conn;
+ 
+	/**
+	 * Singleton access to DB connection to share resources effectively across multiple accesses.
+	 */
+	protected static Connection connect() throws Exception
+	{
+		if (conn != null) { return conn; }
+		
+		try
+		{
+			//System.out.println("start connecting......");
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(
+					jdbcTag + rdsMySqlDatabaseUrl + ":" + rdsMySqlDatabasePort + "/" + dbName + multiQueries,
+					dbUsername,
+					dbPassword);
+			//System.out.println("Database has been connected successfully.");
+			return conn;
+		} catch (Exception ex) {
+			throw new Exception("Failed in database connection");
+		}
+	}
+}
