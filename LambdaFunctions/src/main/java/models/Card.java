5f6aea60aac5f161f93d1e578fb72bf32c8ecd38
Jason King <jpking@wpi.edu>
2019-10-29 20:14:37 -0400

    card class (temp)

--- LambdaFunctions/src/main/java/models/Card.java
+++ LambdaFunctions/src/main/java/models/Card.java
@@ -1,4 +1,22 @@
 package models;
 
+import java.util.HashMap;
+
 public class Card {
+	int cardId;
+	String eventType;
+	String recipient;
+	String orientation;
+	HashMap<Integer, Element> elements = new HashMap<Integer, Element>();
+	
+	public Card(int cardId, String eventType, String recipient, String orientation) {
+		this.cardId = cardId;
+		this.eventType = eventType;
+		this.recipient = recipient;
+		this.orientation = orientation;
+	}
+	
+	public String toGson() {
+		return "nah";
+	}
 }
