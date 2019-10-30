a21fbb6bb04e03d95b871a55f8a11abfda8a65ea
Milap <milap1998@yahoo.com>
2019-10-29 20:52:45 -0400

    constructor overloaded

--- LambdaFunctions/src/main/java/models/Card.java
+++ LambdaFunctions/src/main/java/models/Card.java
@@ -16,6 +16,12 @@ public class Card {
 		this.orientation = orientation;
 	}
 	
+	public Card(String eventType, String recipient, String orientation) {
+		this.eventType = eventType;
+		this.recipient = recipient;
+		this.orientation = orientation;
+	}
+	
 	public String toGson() {
 		return "nah";
 	}
