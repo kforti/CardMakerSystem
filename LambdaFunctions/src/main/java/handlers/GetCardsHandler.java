18aa50a94e8b3af4e8ca080b25461f5e7bf9e62c
Jason King <jpking@wpi.edu>
2019-11-01 18:49:32 -0400

    added Gson dependency, getCards test

--- LambdaFunctions/src/main/java/handlers/GetCardsHandler.java
+++ LambdaFunctions/src/main/java/handlers/GetCardsHandler.java
@@ -5,11 +5,16 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+
+import accessDB.CardsDAO;
+import models.Card;
 import models.GatewayResponse;
 
 /**
@@ -21,16 +26,22 @@ public class GetCardsHandler implements RequestHandler<Object, Object> {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
-        try {
-        	// call getCards using DAO
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            
-            // change this to JSON
 
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
-            
-            return new GatewayResponse(output, headers, 200);
-        } catch (IOException e) {
+    	// get all cards stored in database using DAO
+    	CardsDAO dao = new CardsDAO();
+    	try {
+			List<Card> cards = dao.getAllCards();
+			Gson gson = new Gson();
+			String json = gson.toJson(cards);
+			
+			// return response containing all cards
+            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+            // change this to JSON
+            String output = String.format("{ \"message\": \"%s\", \"location\": \"%s\" }", json, pageContents);
+            return new GatewayResponse(json, headers, 200);
+    	} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
             return new GatewayResponse("{}", headers, 500);
         }
     }
